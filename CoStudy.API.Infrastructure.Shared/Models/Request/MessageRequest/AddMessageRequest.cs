c029015cf225a27cecbd8aafcba0ed42ea7394a9
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2021-01-03 12:36:04 +0700

    .

--- CoStudy.API.Infrastructure.Shared/Models/Request/MessageRequest/AddMessageRequest.cs
+++ CoStudy.API.Infrastructure.Shared/Models/Request/MessageRequest/AddMessageRequest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CoStudy.API.Domain.Entities.Application;
+using Microsoft.AspNetCore.Http;
 using System.Text.Json.Serialization;
 
 namespace CoStudy.API.Infrastructure.Shared.Models.Request.MessageRequest
@@ -12,6 +13,6 @@ namespace CoStudy.API.Infrastructure.Shared.Models.Request.MessageRequest
         public string Content { get; set; }
 
         [JsonPropertyName("image")]
-        public IFormFile Image { get; set; }
+        public Image Image { get; set; }
     }
 }
