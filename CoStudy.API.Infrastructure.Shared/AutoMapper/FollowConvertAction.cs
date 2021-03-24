ec4c75d1ac2ec5d259bc78ef4d7581b0781d1a4f
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2021-03-24 10:23:39 +0700

    update post request

--- CoStudy.API.Infrastructure.Shared/AutoMapper/FollowConvertAction.cs
+++ CoStudy.API.Infrastructure.Shared/AutoMapper/FollowConvertAction.cs
@@ -10,15 +10,33 @@ using System.Text;
 
 namespace CoStudy.API.Infrastructure.Shared.AutoMapper
 {
+    /// <summary>
+    /// Class FollowConvertAction
+    /// </summary>
+    /// <seealso cref="AutoMapper.IMappingAction{CoStudy.API.Domain.Entities.Application.Follow, CoStudy.API.Infrastructure.Shared.ViewModels.FollowViewModel}" />
     public class FollowConvertAction : IMappingAction<Follow, FollowViewModel>
     {
+        /// <summary>
+        /// The user repository
+        /// </summary>
         IUserRepository userRepository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FollowConvertAction"/> class.
+        /// </summary>
+        /// <param name="userRepository">The user repository.</param>
         public FollowConvertAction(IUserRepository userRepository)
         {
             this.userRepository = userRepository;
         }
 
+        /// <summary>
+        /// Implementors can modify both the source and destination objects
+        /// </summary>
+        /// <param name="source">Source object</param>
+        /// <param name="destination">Destination object</param>
+        /// <param name="context">Resolution context</param>
+        /// <exception cref="Exception">Không tìm thấy người theo dõi phù hợp.</exception>
         public void Process(Follow source, FollowViewModel destination, ResolutionContext context)
         {
             var fromUser = userRepository.GetById(ObjectId.Parse(source.FromId));
--- CoStudy.API.Infrastructure.Shared/Models/Request/PostRequest/UpdatePostRequest.cs
+++ CoStudy.API.Infrastructure.Shared/Models/Request/PostRequest/UpdatePostRequest.cs
@@ -4,32 +4,46 @@ using System.Text.Json.Serialization;
 
 namespace CoStudy.API.Infrastructure.Shared.Models.Request.PostRequest
 {
+    /// <summary>
+    /// Class UpdatePostRequest
+    /// </summary>
     public class UpdatePostRequest
     {
+        /// <summary>
+        /// Gets or sets the post identifier.
+        /// </summary>
+        /// <value>
+        /// The post identifier.
+        /// </value>
         [JsonPropertyName("post_id")]
         public string PostId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
         /// <summary>
         /// String content of posts
         /// </summary>
+        /// <value>
+        /// The string contents.
+        /// </value>
         [JsonPropertyName("string_contents")]
         public List<PostContent> StringContents { get; set; }
 
         /// <summary>
         /// Media content of post
         /// </summary>
+        /// <value>
+        /// The media contents.
+        /// </value>
         [JsonPropertyName("image_contents")]
         public List<Image> MediaContents { get; set; }
-
-        /// <summary>
-        /// Lĩnh vực của bài post
-        /// </summary>
-        [JsonPropertyName("fields")]
-        public List<Field> Fields { get; set; }
-
 
     }
 }
